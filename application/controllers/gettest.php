39c830bb67061bffcd108c15318f981b4b94b8a9
Jason Michels <thebizztech@me.com>
2011-11-02 16:35:17 -0500

    Changes to curl and gettest controller

--- application/controllers/gettest.php
+++ application/controllers/gettest.php
@@ -13,35 +13,27 @@ class GetTest extends CI_Controller {
 	{
 		//http://ppipn.jasonmichels.com/ipn_pdt/index.php/gettest/ipntest/
 
-		/*
+		
 		$data['cmd'] = "_notify-validate";
 
 		foreach($this->input->post() as $key => $value)
 		{
 			$data[$key] = $value;
 		}
-		*/
+		
 
-		$req = 'cmd=_notify-validate';
+
+		//$req = 'cmd=_notify-validate';
 		foreach ($_POST as $key => $value) {
 			//$value = urlencode(stripslashes($value));
 			$req .= "&$key=$value";
 		}
 
-		log_message('DEBUG', $req);
 
-		$result = $this->curl->setUrl("https://www.sandbox.paypal.com/cgi-bin/webscr")->setString($req)->post();
-		log_message('DEBUG', $result);
-
-		if($result == "VERIFIED")
-		{
-			log_message('DEBUG', 'It was verified.');
-		}
-		else
-		{
-			log_message('DEBUG', 'It was not verified');
-		}
-		
+		$result = $this->curl
+						->setUrl("https://www.sandbox.paypal.com/cgi-bin/webscr")
+						->setArray($data)
+						->post();		
 
 		$this->load->library('email');
 		
