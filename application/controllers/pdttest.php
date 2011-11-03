1be0a200d4ddcf38c3a712032006039131947f62
Jason Michels <thebizztech@me.com>
2011-11-03 12:02:11 -0500

    PDT testing

--- application/controllers/pdttest.php
+++ application/controllers/pdttest.php
@@ -4,12 +4,14 @@ class PdtTest extends CI_Controller {
 
 	public function index()
 	{
+		$this->load->library('pdt/pdt');
+
 		$data['cmd'] = "_notify-synch";
 		$data['tx'] = $this->input->get('tx');
 		$data['at'] = "anS68XjppePH1P36fFkseWh7FDDyK5iker-R50EKGjd4_ZVDqr9cPbE6fBy";
 
 		$result = $this->curl->setUrl("https://www.sandbox.paypal.com/cgi-bin/webscr")->post($data);
-		$deformat = $this->deformatPDT($result);
+		$deformat = $this->pdt->deformat($result);
 
 		if($deformat === false)
 		{
@@ -30,27 +32,6 @@ class PdtTest extends CI_Controller {
 			}
 		}
 
-	}
-
-	public function deformatPDT($result)
-	{
-		$lines = explode("\n", $result);
-		$keyarray = array();
-		if (strcmp ($lines[0], "SUCCESS") == 0) 
-		{
-			for ($i=1; $i<count($lines);$i++)
-			{
-				list($key,$val) = explode("=", $lines[$i]);
-				$keyarray[urldecode($key)] = urldecode($val);
-			}
-			return $keyarray;
-		}
-		else
-		{
-			//Their was an issue with the request
-			return false;
-		}
-		
 	}
 }
 /* End of file pdttest.php */
