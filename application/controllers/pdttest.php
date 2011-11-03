6bc8962b3deea206ace4c2f1791205d3fe367130
Jason Michels <thebizztech@me.com>
2011-11-03 14:08:22 -0500

    Pdt test changes

--- application/controllers/pdttest.php
+++ application/controllers/pdttest.php
@@ -4,14 +4,12 @@ class PdtTest extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->library('pdt/pdt');
-
 		$data['cmd'] = "_notify-synch";
 		$data['tx'] = $this->input->get('tx');
 		$data['at'] = "anS68XjppePH1P36fFkseWh7FDDyK5iker-R50EKGjd4_ZVDqr9cPbE6fBy";
 
 		$result = $this->curl->setUrl("https://www.sandbox.paypal.com/cgi-bin/webscr")->post($data);
-		$deformat = $this->pdt->deformat($result);
+		$deformat = $this->deformat($result);
 
 		if($deformat === false)
 		{
@@ -37,6 +35,29 @@ class PdtTest extends CI_Controller {
 		echo "</ul>";
 
 	}
+
+	public function deformat($result)
+	{
+		$lines = explode("\n", $result);
+		$keyarray = array();
+
+		//Check to see if request was a success
+		if (strcmp ($lines[0], "SUCCESS") == 0) 
+		{
+			for ($i=1; $i<count($lines);$i++)
+			{
+				list($key,$val) = explode("=", $lines[$i]);
+				$keyarray[urldecode($key)] = urldecode($val);
+			}
+			return $keyarray;
+		}
+		else
+		{
+			//Their was an issue with the request so return false
+			return false;
+		}
+	}
+
 }
 /* End of file pdttest.php */
 /* Location: ./application/controllers/pdttest.php */
